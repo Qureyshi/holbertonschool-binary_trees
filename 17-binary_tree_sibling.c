b5c94b6b632d7b4c8571ffd61a3d2da6dda3b230
mirgubad <Mirgubad@MIRGUBAD>
2024-04-10 23:46:21 +0400

    17 task completed

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -9,4 +9,13 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	 binary_tree_t *sibling;
+
+        if (!node || !node->parent
+        || !node->parent->right || !node->parent->left)
+                return (NULL);
+
+        sibling = node->parent->right;
+        sibling = (node == sibling) ? node->parent->left : sibling;
+        return (sibling);
 }
