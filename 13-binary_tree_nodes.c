3eb555510ef1197bebff32dbbca59b1e213f04c1
Mirgubad <154666707+mirgubadakbarov@users.noreply.github.com>
2024-04-10 23:16:00 +0400

    binary_tree_nodes completed

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,9 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * 
+ * binary_tree_nodes - Counts the nodes in a binary tree
+ * @tree: Pointer to the root node of the tree to count the nodes in
+ *
+ * Return: Number of nodes in the tree, or 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+    size_t size = 0;
 
+    if (!tree || (!tree->left && !tree->right))
+        return (0);
+
+    size += binary_tree_nodes(tree->left);
+    size += binary_tree_nodes(tree->right);
+    return (size + 1);
 }
