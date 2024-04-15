064af79c9b2274f8c365b858f51cb09c1f513532
Queyshi <kamranqureyshi1998@gmail.com>
2024-04-15 12:20:40 +0000

    betty error fixed

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -3,17 +3,15 @@
 /**
  * binary_tree_uncle - Finds the uncle of a node
  *                     in a binary tree.
- * @nodeA pointer to the node to find the uncle of.
+ * @node: pointer to the node to find the uncle of.
  *
  * Return: If node is NULL or has no uncle, NULL.
  *         Otherwise, a pointer to the uncle node.
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
- 	if (node == NULL ||
-	    node->parent == NULL ||
-	    node->parent->parent == NULL)
- 		return (NULL);
+	if (node == NULL || node->parent == NULL || node->parent->parent == NULL)
+		return (NULL);
 	if (node->parent->parent->left == node->parent)
 		return (node->parent->parent->right);
 	return (node->parent->parent->left);
