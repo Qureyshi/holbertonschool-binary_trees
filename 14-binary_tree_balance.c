8b33848ebe0f586733c261431644fcb50f4d2d02
Qureyshi <kamranqureyshi1998@gmail.com>
2024-04-15 11:49:54 +0000

    betty error

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,7 +16,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 /**
  * binary_tree_height - Measures the height of a binary tree.
- * @tr: A pointer to the root node of the tree to measure the height.
+ * @tree: A pointer to the root node of the tree to measure the height.
  *
  * Return: If tree is NULL, your function must return 0, else return height.
  */
